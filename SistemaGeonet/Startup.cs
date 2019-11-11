99e8f033a3d7d810db65df1c1b7259e2b2bdf692
Hans <hans31_1998@hotmail.com>
2019-11-10 21:20:33 -0500

    commit prov

--- SistemaGeonet/Startup.cs
+++ SistemaGeonet/Startup.cs
@@ -61,7 +61,7 @@ namespace SistemaGeonet
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Inventarios}/{action=Index}/{id?}");
             });
         }
     }
