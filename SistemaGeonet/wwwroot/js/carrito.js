b5074a2bb55b4f0b2a6f7fbd0c5e10e57f9a3a3e
Hans <hans31_1998@hotmail.com>
2019-11-20 00:09:28 -0500

    commit amanecida

---
+++ SistemaGeonet/wwwroot/js/carrito.js
@@ -0,0 +1,13 @@
+﻿var list = [];
+function agregarCarritoLS(idEquipo) {
+    list.push(idEquipo);
+    localStorage.setItem("Equipo", list);
+}
+
+
+///*Obtener datos almacenados*/
+//var nombre = localStorage.getItem("Nombre");
+//var apellido = localStorage.getItem("Apellido");
+///*Mostrar datos almacenados*/
+//document.getElementById("nombre").innerHTML = nombre;
+//document.getElementById("apellido").innerHTML = apellido; 
